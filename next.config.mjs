8ebc52506dcc16b44bb7face97ee7760f217aed7
ahmadullah <nikzadahmadullah@gmail.com>
2024-06-24 14:02:19 +0200

    image display solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    webpack5: true,
+    images: {
+        domains: ['https://www.themealdb.com/api.php', 'https://www.themealdb.com/images/ingredients/'],
+    },
+    async headers() {
+        return [
+        {
+            source: '/(.*)',
+            headers: [
+            {
+                key: 'X-Frame-Options',
+                value: 'DENY',
+            },
+            ],
+        },
+        ];
+    },
+};
 
 export default nextConfig;
